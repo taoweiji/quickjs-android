a3f9a197459bcd8a6704cfaf29816d5c35bea155
Wiki <weiji.taoweiji@alibaba-inc.com>
2021-05-23 22:30:12 +0800

    增加executeScript方法

--- quickjs-android/src/main/java/com/quickjs/android/QuickJS.java
+++ quickjs-android/src/main/java/com/quickjs/android/QuickJS.java
@@ -1,5 +1,43 @@
 package com.quickjs.android;
 
 public class QuickJS {
-    public static native int executeIntegerScript(String source,String fileName);
+    private long runtimePtr;
+
+    private QuickJS(long runtimePtr) {
+        this.runtimePtr = runtimePtr;
+    }
+
+    public static QuickJS createV8Runtime() {
+        return new QuickJS(_createRuntime());
+    }
+
+    public JSContext createContext() {
+        return new JSContext(_createContext(runtimePtr));
+    }
+
+    public void close() {
+        _releaseRuntime(runtimePtr);
+    }
+
+    static native long _createRuntime();
+
+    static native void _releaseRuntime(long runtimePtr);
+
+    static native long _createContext(long runtimePtr);
+
+    static native void _releaseContext(long contextPtr);
+
+    static native int _executeIntegerScript(long contextPtr, String source, String fileName);
+
+    static native double _executeDoubleScript(long contextPtr, String source, String fileName);
+
+    static native String _executeStringScript(long contextPtr, String source, String fileName);
+
+    static native boolean _executeBooleanScript(long contextPtr, String source, String fileName);
+
+    static native Object _executeScript(long contextPtr, String source, String fileName);
+
+    static native void _executeVoidScript(long contextPtr, String source, String fileName);
+
+
 }
