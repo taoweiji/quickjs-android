5a1af2b0f06dae8fcfa86f55c87d71713cf2a8d9
Wiki <weiji.taoweiji@alibaba-inc.com>
2021-05-24 01:21:43 +0800

    实现设置值

--- quickjs-android/src/main/java/com/quickjs/android/JSObject.java
+++ quickjs-android/src/main/java/com/quickjs/android/JSObject.java
@@ -15,35 +15,6 @@ public class JSObject extends JSValue {
         this.initialize(this.context.getContextPtr(), data);
     }
 
-
-    public int executeIntegerScript(String source, String fileName) {
-        return QuickJS._executeIntegerScript(this.getContextPtr(), source, fileName);
-    }
-
-    public double executeDoubleScript(String source, String fileName) {
-        return QuickJS._executeDoubleScript(this.getContextPtr(), source, fileName);
-    }
-
-    public String executeStringScript(String source, String fileName) {
-        return QuickJS._executeStringScript(this.getContextPtr(), source, fileName);
-    }
-
-    public boolean executeBooleanScript(String source, String fileName) {
-        return QuickJS._executeBooleanScript(this.getContextPtr(), source, fileName);
-    }
-
-    public Object executeScript(String source, String fileName) {
-        return QuickJS._executeScript(this.getContextPtr(), source, fileName);
-    }
-
-    public void executeVoidScript(String source, String fileName) {
-        QuickJS._executeVoidScript(this.getContextPtr(), source, fileName);
-    }
-
-    public void executeArrayScript(String source, String fileName) {
-        // TODO
-    }
-
     public void add(String key, int value) {
         QuickJS._add(getContextPtr(), this.objectHandle, key, value);
     }
