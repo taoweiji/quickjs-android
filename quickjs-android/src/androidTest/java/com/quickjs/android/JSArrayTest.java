30337c39f415662828fb6e81380110c0ff51ddd9
Wiki <taoweiji2008@qq.com>
2021-05-25 23:30:26 +0800

    test JSObject

--- quickjs-android/src/androidTest/java/com/quickjs/android/JSArrayTest.java
+++ quickjs-android/src/androidTest/java/com/quickjs/android/JSArrayTest.java
@@ -1,11 +1,12 @@
 package com.quickjs.android;
 
-import org.json.JSONArray;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class JSArrayTest {
     JSArray array;
