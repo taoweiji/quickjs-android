b755d14366b46daab0a2b3ce9febb12182343cdd
Wiki <taoweiji2008@qq.com>
2021-05-27 00:36:04 +0800

    test registerJavaMethod

--- quickjs-android/src/androidTest/java/com/quickjs/android/JSFunctionTest.java
+++ quickjs-android/src/androidTest/java/com/quickjs/android/JSFunctionTest.java
@@ -1,12 +1,10 @@
 package com.quickjs.android;
 
-import android.util.Log;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class JSFunctionTest {
 
@@ -42,4 +40,45 @@ public class JSFunctionTest {
         }));
         context.executeVoidScript("test(1, 3.14, true, 'Hello')", "file.js");
     }
+
+    @Test
+    public void call() {
+        context.executeVoidScript("function test(data){return 'Hello'}", "file.js");
+        JSFunction function = (JSFunction) context.getObject("test");
+        String result = (String) function.call(context, null);
+        assertEquals("Hello", result);
+    }
+
+    @Test
+    public void call2() {
+        context.registerJavaMethod(new JavaVoidCallback() {
+            @Override
+            public void invoke(JSArray array) {
+                assertEquals("Hello", array.getString(0));
+            }
+        }, "test");
+        JSFunction function = (JSFunction) context.getObject("test");
+        JSArray args = new JSArray(context);
+        args.push("Hello");
+        args.push(3.14);
+        function.call(context, args);
+    }
+
+    @Test
+    public void call3() {
+        context.registerJavaMethod(new JavaCallback() {
+            @Override
+            public Object invoke(JSArray array) {
+                return array.getString(0);
+            }
+        }, "test");
+        JSFunction function = (JSFunction) context.getObject("test");
+        JSArray args = new JSArray(context);
+        args.push("Hello");
+        args.push(3.14);
+        String result = (String) function.call(context, args);
+        assertEquals("Hello", result);
+    }
+
+
 }
