4ffa99027812c3e9ebc8a8cc65740a68688377c6
Wiki <weiji.taoweiji@alibaba-inc.com>
2021-06-04 00:44:37 +0800

    checkReleased
    Took 9 minutes

--- quickjs-android/src/main/java/com/quickjs/QuickJS.java
+++ quickjs-android/src/main/java/com/quickjs/QuickJS.java
@@ -116,17 +116,7 @@ public class QuickJS implements Closeable {
         throw new QuickJSException(result[0], message.toString());
     }
 
-    void checkRuntime(JSValue value) {
-        if (value != null && !value.isUndefined()) {
-            if (value.context == null) {
-                throw new Error("Invalid target runtime");
-            }
-            QuickJS quickJS = value.context.quickJS;
-            if (quickJS == null || quickJS.isReleased() || quickJS != this) {
-                throw new Error("Invalid target runtime");
-            }
-        }
-    }
+
 
     public boolean isReleased() {
         return this.released;
