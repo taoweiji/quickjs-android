2df2b460ec9bd9282e15f2c11118f615fe4253d6
Wiki <taoweiji2008@qq.com>
2021-06-09 09:05:14 +0800

    实现EventQueue
    Took 32 minutes

--- quickjs-android/src/androidTest/java/com/quickjs/EventQueueTest.java
+++ quickjs-android/src/androidTest/java/com/quickjs/EventQueueTest.java
@@ -5,28 +5,39 @@ import com.quickjs.plugin.ConsolePlugin;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class EventQueueTest {
-    EventQueue eventQueue;
+    EventQueue<ES6Module> eventQueue;
+    List<String> logs = new ArrayList<>();
 
     @Before
     public void setUp() {
-        eventQueue = new EventQueue() {
+        eventQueue = new EventQueue<ES6Module>() {
             @Override
-            protected void setup(JSContext context) {
+            protected void setup(ES6Module context) {
                 super.setup(context);
                 context.addPlugin(new ConsolePlugin() {
                     @Override
                     public void println(int priority, String msg) {
                         super.println(priority, msg);
+                        logs.add(msg);
                     }
                 });
             }
 
             @Override
-            protected JSContext createContext(QuickJS quickJS) {
-                return super.createContext(quickJS);
+            protected ES6Module createContext(QuickJS quickJS) {
+                return new ES6Module(quickJS) {
+
+                    @Override
+                    protected String getModuleScript(String moduleName) {
+                        return null;
+                    }
+                };
             }
         };
         eventQueue.start();
@@ -34,35 +45,55 @@ public class EventQueueTest {
 
     @Test
     public void post() throws InterruptedException {
-        eventQueue.post(context -> context.executeVoidScript("var timer = 0;\n" +
-                "function print1() {\n" +
-                "    console.log(timer++);\n" +
-                "    setTimeout(print2, 1000);\n" +
-                "}\n" +
-                "function print2() {\n" +
-                "    console.log(timer++);\n" +
-                "    if(timer < 1000){\n" +
-                "        setTimeout(print1, 1000);\n" +
-                "    }\n" +
-                "}\n" +
-                "print1();", null));
-        eventQueue.quitSafely(20000);
+        eventQueue.post(new EventQueue.Event<ES6Module>() {
+            @Override
+            public void on(ES6Module context) {
+                context.executeGlobalScript("var counter = 0;\n" +
+                        "function print1() {\n" +
+                        "    console.log(counter++);\n" +
+                        "    setTimeout(print1, 10);\n" +
+                        "}\n" +
+                        "print1();", null);
+            }
+        });
+        eventQueue.join(1000);
+        eventQueue.quit();
+        assertTrue(logs.size() > 10);
     }
 
     @Test
     public void quitSafely() throws InterruptedException {
-        eventQueue.post(context -> context.executeVoidScript("console.log('Hello')", null));
-        eventQueue.quitSafely(1000);
+        eventQueue.post(new EventQueue.Event<ES6Module>() {
+            @Override
+            public void on(ES6Module context) {
+                context.executeGlobalScript("console.log('Hello')", null);
+            }
+        });
+        eventQueue.quitSafely();
+        assertEquals(1, logs.size());
     }
 
     @Test
     public void quit() {
-        eventQueue.post(context -> context.executeVoidScript("console.log('Hello')", null));
+        eventQueue.post(new EventQueue.Event<ES6Module>() {
+            @Override
+            public void on(ES6Module context) {
+                context.executeGlobalScript("console.log('Hello')", null);
+            }
+        });
         eventQueue.quit();
+        assertEquals(0, logs.size());
     }
 
     @Test
     public void postDelayed() throws InterruptedException {
-//        eventQueue.join();
+        eventQueue.postDelayed(new EventQueue.Event<ES6Module>() {
+            @Override
+            public void on(ES6Module context) {
+                context.executeGlobalScript("console.log('Hello')", null);
+            }
+        }, 1000);
+        eventQueue.quitSafely();
+        assertEquals(0, logs.size());
     }
 }
